7e6560071d749fdd15283664aa7788c1bd7aa95a
qoeka98 <vhzkflfltm6@gmail.com>
2025-02-18 15:56:25 +0900

    1

---
+++ data.py
@@ -0,0 +1,11 @@
+
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.records.WeightRecord
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
+import java.time.Instant
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
